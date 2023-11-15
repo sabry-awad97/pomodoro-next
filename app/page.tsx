cc1fba181d786a007a7519a2d14c7b9cdcabd6fd
Sabry Awad <dr.sabry1997@gmail.com>
2023-11-15 23:54:28 +0200

    ✨ feat(app): Add Pomodoro timer functionality and reset option 🔄 refactor(app): Refactor timer logic to useInterval custom hook 🚚 chore(package.json): Add Tauri API dependencies 🚀 chore(src-tauri): Update Tauri dependencies and configure notifications/dialogs

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,83 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { useInterval } from './hooks/useInterval';
+import { ask } from '@tauri-apps/api/dialog';
+import { sendNotification } from '@tauri-apps/api/notification';
 
-const buttons = [
+const BUTTON_TIMES = [
   {
-    value: 900,
+    value: 15 * 60,
     display: '15 minutes',
   },
   {
-    value: 1800,
+    value: 30 * 60,
     display: '30 minutes',
   },
   {
-    value: 3600,
+    value: 60 * 60,
     display: '60 minutes',
   },
 ];
 
+const initialTimerValue = 15 * 60; // Initial time set to 15 minutes
+
 function App() {
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState(initialTimerValue);
   const [timerStart, setTimerStart] = useState(false);
 
   const toggleTimer = () => {
     setTimerStart(!timerStart);
   };
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (timerStart) {
-        if (time > 0) {
-          setTime(time - 1);
-        } else if (time === 0) {
-          // TODO: Send notification to the user.
-          clearInterval(interval);
-        }
+  const resetTimer = async () => {
+    const shouldReset = await ask('Do you want to reset timer?', {
+      title: 'Pomodoro Timer App',
+      type: 'warning',
+    });
+
+    if (shouldReset) {
+      setTime(initialTimerValue);
+      setTimerStart(false);
+    }
+  };
+
+  useInterval(() => {
+    if (timerStart) {
+      if (time > 0) {
+        setTime(prevTime => prevTime - 1);
+      } else {
+        sendNotification({
+          title: `Time's up!`,
+          body: `Congrats on completing a session!🎉`,
+        });
+        setTimerStart(false);
       }
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [timerStart, time]);
+    }
+  }, 1000);
 
   return (
     <div className="min-h-screen bg-gray-700 flex items-center flex-col">
       <h1 className="text-white font-bold mt-20 text-5xl">Pomodoro Timer</h1>
-      <div className="text-white font-bold text-7xl mt-5">
-        {`${
-          Math.floor(time / 60) < 10
-            ? `0${Math.floor(time / 60)}`
-            : Math.floor(time / 60)
-        }:${time % 60 < 10 ? `0${time % 60}` : time % 60}`}
-      </div>
+      <div className="text-white font-bold text-7xl mt-5">{`${String(
+        Math.floor(time / 60)
+      ).padStart(2, '0')}:${String(time % 60).padStart(2, '0')}`}</div>
       <div className="flex mt-5">
         <button
-          className="w-28 bg-red-700 text-white py-2 px-4 rounded"
+          className="w-28 bg-red-500 text-white py-2 px-4 rounded"
           onClick={toggleTimer}
         >
           {!timerStart ? 'Start' : 'Pause'}
         </button>
-        {/* TODO: Add Button to reset timer */}
+        <button
+          className="bg-blue-300 mx-5 py-2 px-4 rounded"
+          onClick={resetTimer}
+        >
+          Reset
+        </button>
       </div>
       <div className="flex mt-10">
-        {buttons.map(({ value, display }) => (
+        {BUTTON_TIMES.map(({ value, display }) => (
           <button
             key={value}
             className="mx-4 bg-green-300 text-white py-2 px-4 rounded"
